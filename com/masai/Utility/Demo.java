5137e96d4e272f07081390b5397928e00f2e807e
Mansi Deshmukh <102024693+Mansi-Deshmukh@users.noreply.github.com>
2022-10-02 01:37:07 +0530

    Add files via upload

---
+++ com/masai/Utility/Demo.java
@@ -0,0 +1,15 @@
+package com.masai.Utility;
+
+import java.sql.Connection;
+
+public class Demo {
+
+	
+	public static void main(String[] args) {
+		Connection conn= DButility.provideConnection();
+		
+		
+		System.out.println(conn);
+	}
+
+}
