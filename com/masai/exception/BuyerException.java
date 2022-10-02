5c5974b1d99b3ecbb61a5afdc5c7da42c83dc006
Mansi Deshmukh <102024693+Mansi-Deshmukh@users.noreply.github.com>
2022-10-02 13:13:37 +0530

    Add files via upload

---
+++ com/masai/exception/BuyerException.java
@@ -0,0 +1,12 @@
+package com.masai.exceptions;
+
+public class BuyerException extends Exception{
+
+	public BuyerException() {
+		
+	}
+   public BuyerException(String msg) {
+		super(msg);
+	}
+	
+}
---
+++ com/masai/exception/SellerException.java
@@ -0,0 +1,14 @@
+package com.masai.exceptions;
+
+public class SellerException extends Exception {
+
+	public SellerException() {
+		
+	}
+	
+   public SellerException(String msg) {
+		super(msg);
+	}
+	
+	
+}
